409cfb4484da7fde5209d12deaf6e49ed28c2233
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-07-08 11:15:45 +0300

    Add Coords with one useState

--- src/components/Coords/index.jsx
+++ src/components/Coords/index.jsx
@@ -4,17 +4,30 @@ const Coords = () => {
   const [coords, setCoords] = useState({
     x: 0,
     y: 0,
+    countClick: 0
   });
-  const handleClick = (event) => {
+  const handleMouseMove = (event) => {
     setCoords({
+      ...coords,
       x: event.clientX,
       y: event.clientY,
     });
   };
+  const handleClick = () => {
+    setCoords({
+      ...coords,
+      countClick: coords.countClick + 1,
+    });
+  };
   return (
-    <div onClick={handleClick} style={{background:'#eee'}}>
+    <div
+      onMouseMove={handleMouseMove}
+      onClick={handleClick}
+      style={{ background: '#eee' }}
+    >
       <p>x = {coords.x}</p>
       <p>y = {coords.y}</p>
+      <p>countClick = {coords.countClick}</p>
     </div>
   );
 };
