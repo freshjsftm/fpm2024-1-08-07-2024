5f77127b228c547e9c01ea8494b9f2877213ab0e
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-07-08 10:25:08 +0300

    Add Counter

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,5 +1,20 @@
+import { useState } from 'react';
+
 const Counter = () => {
-  return <div>Counter</div>;
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+  };
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button onClick={increment}>add 1</button>
+      <button onClick={decrement}>sub 1</button>
+    </div>
+  );
 };
 
 export default Counter;
