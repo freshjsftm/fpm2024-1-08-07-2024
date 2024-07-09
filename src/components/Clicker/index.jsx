b30cfc97c34e6011a2ec43be1351f2f3d3bcf613
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-07-09 10:05:22 +0300

    Refactor Clicker

--- src/components/Clicker/index.jsx
+++ src/components/Clicker/index.jsx
@@ -3,18 +3,18 @@ import './Clicker.css';
 
 const Clicker = () => {
   const [clickCount, setClickCount] = useState(0);
-  const handleClick = () => {
-    setClickCount(clickCount + 1);
-  };
 
   useEffect(() => {
+    const handleClick = () => {
+      setClickCount((clickCount) => clickCount + 1);
+    };
     console.log('add effect');
     document.body.addEventListener('click', handleClick);
     return () => {
       console.log('remove effect');
       document.body.removeEventListener('click', handleClick);
     };
-  });
+  }, []);
 
   console.log('render');
   return <h2>Clicker: {clickCount}</h2>;
