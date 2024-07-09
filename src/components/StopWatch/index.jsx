8663a01f185ff9cff67934867f1c927ebf40b2e8
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-07-09 12:23:43 +0300

    Add BUG hook

--- src/components/StopWatch/index.jsx
+++ src/components/StopWatch/index.jsx
@@ -1,11 +1,31 @@
-
+import { useState, useEffect } from 'react';
 
 const StopWatch = () => {
+  const [time, setTime] = useState(0);
+  const [isRuning, setIsRuning] = useState(false);
+
+  const handleIsRuning = () => {
+    setIsRuning(!isRuning);
+  };
+  if (isRuning) { //!!! так робити не можна!!!
+    useEffect(() => {
+      console.log('add setInterval');
+      const idInterval = setInterval(() => {
+        setTime((time) => time + 1);
+      }, 1000);
+      return () => {
+        clearInterval(idInterval);
+        console.log('clear Interval');
+      };
+    }, []);
+  }
+
   return (
     <div>
-      
+      <h2>{time}</h2>
+      <button onClick={handleIsRuning}>{isRuning ? 'stop' : 'start'}</button>
     </div>
   );
-}
+};
 
 export default StopWatch;
