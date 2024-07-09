e6a673324859da54344f1927ae66c21de9a31820
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-07-09 09:54:32 +0300

    Add useEffect to Clicker

--- src/components/Clicker/index.jsx
+++ src/components/Clicker/index.jsx
@@ -1,11 +1,24 @@
-
+import { useState, useEffect } from 'react';
+import './Clicker.css';
 
 const Clicker = () => {
-  return (
-    <div>
-      Clicker
-    </div>
-  );
-}
+  const [clickCount, setClickCount] = useState(0);
+  const handleClick = () => {
+    setClickCount(clickCount + 1);
+  };
+
+  useEffect(() => {
+    console.log('add effect');
+    document.body.addEventListener('click', handleClick);
+    return () => {
+      console.log('remove effect');
+      document.body.removeEventListener('click', handleClick);
+    };
+  });
+
+  console.log('render');
+  return <h2>Clicker: {clickCount}</h2>;
+};
 
 export default Clicker;
+//  onClick={handleClick}
