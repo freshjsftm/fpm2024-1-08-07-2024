888f8081c9fd266a31579971239c017024327e50
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-07-11 10:04:04 +0300

    Add ThemeContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
+import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import { UserContext, ThemeContext } from './contexts';
 import HomePage from './pages/HomePage';
-
 import Coords from './components/Coords';
 import Header from './components/Header';
 
 function App() {
+  const [user] = useState({
+    id: 5,
+    login: 'max',
+    avatar: '/images/defava.png',
+  });
+  const [theme, setTheme] = useState('light');
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/coords" element={<Coords />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeContext.Provider value={[theme, setTheme]}>
+      <UserContext.Provider value={user}>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/coords" element={<Coords />} />
+          </Routes>
+        </BrowserRouter>
+      </UserContext.Provider>
+    </ThemeContext.Provider>
   );
 }
 
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,25 @@
+import { useContext } from 'react';
+import styles from './Header.module.scss';
+import { UserContext, ThemeContext } from '../../contexts';
 import NavMenu from '../NavMenu';
 
 const Header = () => {
+  const { login, avatar } = useContext(UserContext);
+  const [theme, setTheme] = useContext(ThemeContext);
+  console.log(theme);
+  const handleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
   return (
-    <header>
+    <header className={styles.header}>
       <NavMenu />
+      <button onClick={handleTheme}>
+        {theme === 'light' ? 'dark' : 'light'}
+      </button>
+      <span>
+        Hi, {login}
+        <img src={avatar} alt="avatar" />
+      </span>
     </header>
   );
 };
